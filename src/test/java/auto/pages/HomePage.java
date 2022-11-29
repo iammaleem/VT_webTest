d6931333b5e7e7281a43eda4fcd103dbe681e8c1
iammaleem <69584151+iammaleem@users.noreply.github.com>
2022-11-29 11:53:47 +0500

    Update and rename WelcomePage.java to HomePage.java

--- src/test/java/auto/pages/HomePage.java
+++ src/test/java/auto/pages/HomePage.java
@@ -1,21 +1,32 @@
-package auto.pages;
+package page.Objects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 
-public class WelcomePage {
+public class HomePage {
 
-    final static String TITLE = "The Internet";
-    final static String HEADING = "Welcome to the-internet";
+    final static String TITLE = "VdoTok - Easy-to-use APIs for Live Media";
+    final static String HEADING = "Need easy-to-use\n"
+    		+ "APIs for live media?";
     WebDriver driver;
-    String xpathHeading = "//h1";
+    String xpathHeading = "//p[@class='typekit_h1 webkit_primary_h h1bp']";
     String xpathLink = "//ul/li/a[text()='**link**']";
+    String xpathLogo =  "img[id='vdtbrandlogo']";
 
-    public WelcomePage(WebDriver driver) {
+    String TryfreeButton = "//button[contains(@class, 'tryfree')]";
+  
+   // String currentPageSource = driver.getPageSource();
+
+    
+    
+
+
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
@@ -32,9 +43,14 @@ public class WelcomePage {
                 "Actual heading '" + actualHeading + "' should be same as expected '" + HEADING + "'.");
     }
 
-    public void clickOnLinkViaLinkText(String link) {
-        driver.findElement(By.linkText(link)).click();
+    public void clickOnLogo() {
+        driver.findElement(By.cssSelector(xpathLogo)).click();
     }
+
+    public void clickOnTryFree() {
+        driver.findElement(By.xpath(TryfreeButton)).click();
+    } 
+
 
     public void clickOnLinkViaPartialText(String link) {
         driver.findElement(By.partialLinkText(link)).click();
